79bac94e90a540a5b4cf0a52fbfddbc61bac6291
Igor <arshavik54@rambler.ru>
2023-09-22 00:00:52 +0500

    Добавил Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два 
+// числа и выдает, какое число больше, а какое меньше.
+// a = 5; b = 7 -> max = 7;
+// a = 2; b = 10 -> max = 10;
+// a = -9; b = -3 -> max = -3;
+
+int a = -9;
+int b = -3;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
