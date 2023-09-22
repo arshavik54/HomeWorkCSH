0031e201f68946738415b98bb45ac534f51f478b
Igor <arshavik54@rambler.ru>
2023-09-22 15:20:14 +0500

    Добавил Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает
+// все четные числа от 1 до N.
+// Например:
+// 5 -> 2, 4;
+// 8 -> 2, 4, 6, 8;
+
+Console.WriteLine("Введите число > 0");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0) ;
+int count = 1;
+{
+     while (count < number)
+     {
+        count % 2 == 1;
+        count += 1;
+     }
+}
+else 
+{
+Console.Write($"{count} ");   
+}
+ 
+
+
+
+
