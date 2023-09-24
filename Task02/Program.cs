b4d4b1f72f4da1ffede42dfb0c38018491ab4ae3
Igor <arshavik54@rambler.ru>
2023-09-25 01:20:42 +0500

    Исправил задачу 02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -4,10 +4,11 @@
 // a = 2; b = 10 -> max = 10;
 // a = -9; b = -3 -> max = -3;
 
-int a = -9;
-int b = -3;
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
 int max = a;
-if (a > max) max = a;
 if (b > max) max = b;
 Console.Write("max = ");
 Console.WriteLine(max);
