6d72274d981eb893342b9201d51e23029f53da5f
Igor <arshavik54@rambler.ru>
2023-09-25 16:48:58 +0500

    добавил Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,3 +1,39 @@
-﻿// Задача 13: Напишите программу, которая выводит третью
-// цифру заданного числа или сообщает, что третьей цифры
-// нет.
+﻿// Задача 13: Напишите программу, которая 
+// 1. выводит третью цифру заданного числа или 
+// 2. сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Prompt (string message)
+{   
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("Введите число > ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));
+}
