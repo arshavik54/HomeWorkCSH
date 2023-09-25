2ac29c473d35107932701cfdb2f935e582d448be
Igor <arshavik54@rambler.ru>
2023-09-25 16:14:11 +0500

    добавил Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,10 +5,11 @@
 // 12-> 2
 // 85 -> 8
 
-// using Internal;
-
 int number = new Random().Next(10, 100); //99+1
 Console.WriteLine($"Случайное число из отрезка [10, 99] -> {number}");
 
 int firstDigit = number / 10; // 78 / 10 = 7
 int secondDigit = number % 10; // 78 = 70 + 8
+
+if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,3 +1,24 @@
-﻿// 10: Напишите программу, которая принимает на
-// вход трёхзначное число и на выходе показывает вторую
-// цифру этого числа.
+﻿// 10: Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и 
+// 2. на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt (string message)
+{   
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трехзначное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
+    return;
+}
+Console.WriteLine($"Введенное число '{number}'");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра '{secondRank}'");
