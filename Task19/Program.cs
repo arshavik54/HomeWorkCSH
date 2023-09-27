fa57919edfd8ed9b593b5a2659eb48c7dd5c73e1
Igor <arshavik54@rambler.ru>
2023-09-27 21:45:18 +0500

    Продолжил 19 задачу

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,27 +5,20 @@
 // 23432 -> да.
 // 12821 -> да.
 
-int GetUserInput(string str)
-// (string str -> потому что дальше пишем "Введите число")
-{
-    Console.Write($"{str}: ");
-    int num = Convert.ToInt32(Console.ReadLine());
-    return num;
-}
-bool IsPalindrome(int number)
-{
-    int firstDigit == int fourthDigit;
-    int secondDigit == int fifthDigit;
-    return IsPalindromePalindrom(int number);
-}
-bool NoPalindrome(int number)
-{
-    if (number > 9999 && number < 100000)
+bool fifthsDigit (int number)
+
+if (number > 9999 && number < 100000)
     { 
         return true
     }
-    Console.WriteLine("Вы ввели не пятизначное число");
+    Console.WriteLine("число не пятизначное");
     return false
+
+bool ValidatePalindrome(int num)
+{
+    int firstDigit == int fifthDigit;
+    int secondDigit == int fourthDigit ;
+    return IsPalindrom(int number);
 }
 
 int firstDigit = number % 10000;
@@ -34,4 +27,4 @@ int result = FirstDigit == FourthDigit && SecondDigit == FifthDigit
 int number = GetUserInput("Введите пятизначное число: ");
 
 bool palindrom = Palindrom(number)  
-Console.WriteLine(IsPalindrome ? "да" : "нет");
+Console.WriteLine(IsPalindrome ? "true" : "false");
