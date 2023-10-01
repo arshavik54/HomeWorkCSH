fc958ba5c82abadd65e1c55997a05a2475e8fd8f
Igor <arshavik54@rambler.ru>
2023-10-01 11:34:27 +0500

    ННаписал 21 задачу

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,25 +6,24 @@
 // 23432 -> да.
 // 12821 -> да.
 
-int GetUserInput(string str)
-{
-    Console.Write($"{str}: ");
+    Console.WriteLine("Введите пятизначное число");
     int number = Convert.ToInt32(Console.ReadLine());
-    return number;
-}
 
-bool FifthsDigit(int number)
+bool FifthDigit(int number)
 { 
-    if (number > 9999 && number < 100000)
+    if (number > 9999 && number < 99999)
     {
         return true;
     }
-    Console.WriteLine("число не пятизначное");
+    else 
+    {
+    Console.Write("Число не пятизначное");
     return false;    
+    }
+    
 }
-
-bool ValidatePalindrome(int number)
-{
+    
+bool IsPalindrome(int number)
     {
         if ((number / 10000 == number % 10) && 
         number /1000 % 10 == number % 100 /10)
@@ -37,14 +36,17 @@ bool ValidatePalindrome(int number)
         }
         
     }
-}
-int number = GetUserInput("Введите пятизначное число");
 
-bool result = ValidatePalindrome(number);
-Console.WriteLine($"{result}",  - > True )
+// int number = GetUserInput("Введите пятизначное число");
+bool fifthDigit = FifthDigit(number);
+Console.WriteLine(fifthDigit  ? "Число не пятизначное");
+
+bool result = IsPalindrome(number);
+Console.WriteLine(result ? true : false);
 
 // int firstDigit = number / 10000;
 // int fifthDigit = number % 10;
 // int secondDigit = number / 1000 % 10;
 // int fourthDigit = number % 100 /10;
 
+ 
--- Task21/Program.cs
+++ Task21/Program.cs
@@ -4,3 +4,39 @@
 // A (3, 6, 8); B (2, 1, -7) -> 15.84;
 // A (7, -5, 0); B (1, -1, 9) -> 11.53
 
+// int n = 5; 
+// int nS = n * n;
+// double nS2 = Math.Pow(n, 2);
+
+  
+int GetUserInput(string message)
+{
+    Console.Write($"{message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+  // d = √((х2 –  х1)2 + (у2 – у1)2) +(z2 - z1)2
+double Distance(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+    return res;
+}
+int x1 = GetUserInput("Координаты первой точки\nВведите координату X");
+int y1 = GetUserInput("Координаты первой точки\nВведите координату Y");
+int z1 = GetUserInput("Координаты первой точки\nВведите координату Z");
+Console.WriteLine("------------------------------");
+int x2 = GetUserInput("Координаты второй точки\nВведите координату X");
+int y2 = GetUserInput("Координаты второй точки\nВведите координату Y");
+int z2 = GetUserInput("Координаты первой точки\nВведите координату Z");
+
+
+double result = Distance(x1, y1, x2, y2, z1, z2);
+Console.WriteLine(Math.Pow(result, 2));
+  
+
+  
+        // Не удаляйте строки ниже
+        
+        // result = (new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        // Console.WriteLine($"{result:F2}");
+  
