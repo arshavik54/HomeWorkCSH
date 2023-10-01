906f34fb135691621ea12ce62a0778056cef25c9
Igor <arshavik54@rambler.ru>
2023-10-01 14:25:32 +0500

    Закончил 23 задачу

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -3,3 +3,16 @@
 // 2. выдает таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27.
 // 5 -> 1, 8, 27, 64, 125.
+
+void CubeNumbers (int num) 
+{
+    int count = 1;
+    while (count <= num) {
+        Console.WriteLine($"{count, 3} -> {count*count*count, 5}");
+        count++;
+    }
+}
+
+Console.WriteLine ("Введите число: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+CubeNumbers(userNum);
