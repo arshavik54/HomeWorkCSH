73862935e2dcce265469c71bc369493a8d342dfa
Igor <arshavik54@rambler.ru>
2023-10-04 01:36:42 +0500

    Написал 27-ю задачу

---
+++ Task27/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 27: Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// int Prompt (string message)
+// {
+//     System.Console.Write(message);
+//     string readInput = System.Console.ReadLine();
+//     int result = int.Parse(readInput);
+//     return result;
+// }
+
+// int SumAllDigit(int number)
+// {
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         result += number % 10;
+//         number = number / 10;
+//     }
+//     return result;
+// } 
+
+
+int SumDigit (int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = SumDigit(number);
+
+Console.WriteLine($"сумма цифр в числе {number} = {sum} ");     
+// int number = Prompt("Введите число");
+//  System.Console.WriteLine($"Сумма всех чисел в цифре {number} = {SumAllDigit(number)}");
