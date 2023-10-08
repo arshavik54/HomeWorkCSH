d273a4b2f2ae49887ae3ec295eb8138d54e8885e
Igor <arshavik54@rambler.ru>
2023-10-08 14:08:38 +0500

    решил 34 задачу

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -4,27 +4,17 @@
 // массиве.
 // [345, 897, 568, 234] -> 2
 
+
 int[] CreateCustomArray(int size, int min, int max)
 {
-    int[] arr = new int[size];
-    Random rnd = new();
-    for (int i =0; i < size; i++){
-        arr[i] = rnd.Next(min, max);
-    }
-    return arr;
+int[] arr = new int[size];
+Random rnd = new();
+for (int i =0; i < size; i++){
+    arr[i] = rnd.Next(min, max);
+}
+return arr;
 }
 
-int EvenDigits(int[]arr)
-{
-int count = 0;    
-int number = 0;
-for (int i = 0; i < arr.Length; i++)
-{
-if  (number % 2 == 0);
-    return count +=1;
-}    
-
-}
 void PrintOut(int[] arr)
 {
 for (int i = 0; i < arr.Length - 1; i++)
@@ -33,6 +23,16 @@ Console.Write($"{arr[i]}, ");
 }
 Console.Write($"{arr[^1]}");// ^1 последний элемент массива
 }
+
+int EvenDigits(int[] arr)
+{    
+int count = 0;    
+for (int i = 0; i < arr.Length; i++);
+{
+if (arr[i] % 2 == 0) count ++;
+}
+return count;
+}  
 
 int[] randArr = CreateCustomArray(4, 100, 999);
 PrintOut(randArr);
