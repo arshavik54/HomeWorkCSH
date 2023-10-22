44e96f2418aff7dee931fc3867de4fe4ed3e3fc5
Igor <arshavik54@rambler.ru>
2023-10-22 23:07:12 +0500

    доделал 66 задачу

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -3,7 +3,6 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-
 int UserNumber(string message)
 {
 Console.WriteLine(message);
@@ -13,13 +12,17 @@ return num;
 
 int SumNaturalsNumbers(int m, int n)// 0,15;
 {
-    if (m == 0) return 0; ;
+    if (m == n) return m;
     return n + SumNaturalsNumbers(m, n - 1);
-    // 0+15+14+13+..
 }  
-
 
 int m = UserNumber("Введите число M: ");
 int n = UserNumber("Введите число N: ");
 int num = SumNaturalsNumbers(m, n);
-Console.WriteLine($"Сумма элементов равна: {num}");
+
+int PowerMN = 0;
+if (m < n)
+PowerMN = SumNaturalsNumbers(m, n);
+else
+PowerMN = m + SumNaturalsNumbers(n, m - 1);
+Console.WriteLine($"Сумма элементов равна: {PowerMN}");
