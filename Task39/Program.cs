bb36cd035e75b6ae77b33a49ca625a6699de0f77
Igor <arshavik54@rambler.ru>
2023-11-30 01:58:14 +0500

    отредактировал 39 задачу

--- Task39/Program.cs
+++ Task39/Program.cs
@@ -4,9 +4,32 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
+int[] CreateArrarRndInt(int size, int min, int max)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+
+for (int i = 0; i < size; i++)
+{
+arr[i] = rnd.Next(min, max + 1);
+}
+return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
 void ReverseArray(int[] arr)
 {
-    for (int i = 1; i < arr.Length / 2; i++)
+    for (int i = 0; i < arr.Length / 2; i++)
     {
         int temp = arr[i];// 2
         arr[i] = arr[arr.Length - 1 - i];
