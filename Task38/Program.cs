0b7250792c12a2a4de44a7c30de2f127645c4886
Igor <arshavik54@rambler.ru>
2023-11-30 01:57:37 +0500

    решаю 38 задачу

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -2,49 +2,6 @@
 // между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
-double[] CreateCustomArray(int size, int min, int max)
-{
-    double[] arr = new double[size];
-    Random rnd = new Random();
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = rnd.NextDouble() * (max - min) + min;
-    }
-    return arr;
-}
-
-double MaxDigit(double[] arr, double number)
-{
-     Double Max = number;
-     for (double i = 0; i < arr.Length; i++)
-     if (i > Max) i = Max;
-    return Max;
-}
-
-double MinDigit(double[] arr, double number)
-{
-     Double Min = number;
-     for (double i = 0; i < arr.Length; i++)
-     if (i < Min) i = Min;
-    return Min;
-}
-
-double Difference(int maxdigit, int mindigit)
-{
-maxdigit = MaxDigit; mindigit = MinDigit;
-difference = Difference; 
-
-}
-void PrintArrayDouble(double[] arr)
-{
-    for (int i = 0; i < arr.Length - 1; i++)
-    {
-        Console.Write($"{arr[i]}:F1, ");
-    }
-    Console.Write($"{arr[^1]}]:F1 {MaxDigit} - {MinDigit} -> {Difference}");// ^1 последний элемент массива
-}
-
-
 double[] CreateArrarRndDouble(int size, int min, int max)
 {
     double[] arr = new double[size];
@@ -54,9 +11,51 @@ double[] CreateArrarRndDouble(int size, int min, int max)
     {
         arr[i] = rnd.NextDouble() * (max - min) + min;
     }
-
     return arr;
 }
+
+
+double MaxDigit(double[] arr, double maxNum)
+{
+     Double max = maxNum;
+     for (double i = 0; i < arr.Length; i++)
+     {
+         if (i > max) i = max;
+     }
+    return max;
+}
+
+double MinDigit(double[] arr, double minNum)
+{
+     Double min = minNum;
+     for (double i = 0; i < arr.Length; i++)
+     {
+         if (i < min) i = min;
+     }
+    return min;
+}
+double DifferenceDigits(double MaxNum, double MinNum, double result)
+{
+    result = MaxNum - MinNum;
+    return result;
+}
+
+void PrintArrayDouble(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]:F2}; ");     // F2 - знаки после запятой
+    }
+    Console.Write($"{arr[^1]:F2}] {MaxDigit} - {MinDigit} -> {DifferenceDigits}");// ^1 последний элемент массива
+}
+   
+double[] randArr  = CreateArrarRndDouble(6, 1, 10);
+PrintArrayDouble(randArr);
+
+
+
+
 
 // double[] CreateArrarRndDouble(int size, int min, int max)
 // {
